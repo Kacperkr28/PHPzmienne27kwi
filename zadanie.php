23ee2cc3a50d70a1e26c8dd03f665fbf042a9417
Kacperkr28 <krawczyk.kacper@zst.radom.pl>
2026-04-27 09:08:34 +0200

    Add files via upload

---
+++ zadanie.php
@@ -0,0 +1,38 @@
+<?php
+echo "Zadania: <br>";
+
+// 1
+$a = 12; 
+echo $a . "<br>";
+
+// 2
+$a = -$a; 
+echo $a . "<br>";
+
+// 3
+$a += 3; 
+echo $a . "<br>";
+
+// 4
+$a -= 5; 
+echo $a . "<br>";
+
+// 5
+$a *= 2; 
+echo $a . "<br>";
+
+// 6
+$a /= 5; 
+echo $a . "<br>";
+
+// 7
+$a = fmod($a, 2); 
+echo $a . "<br>";
+
+// 8
+$a = pow($a, 3); 
+echo $a . "<br>";
+
+// 9
+echo "Wartość zmiennej \$a wynosi: " . $a . " i już się nie zmieni.";
+?>
